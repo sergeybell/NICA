6e4fe982bd40af1b5d8dcd355f2aa1b934840661
sergeybell <sergey.bell13@gmail.com>
2020-10-08 21:01:01 +0300

    1) add OLD stucture without rename; 2) add in 15,5 NICA compenlation sextupoles

--- MADX/2.Programms/Plot/Plot Phase Space NICA g=13 xpx.py
+++ MADX/2.Programms/Plot/Plot Phase Space NICA g=13 xpx.py
@@ -5,8 +5,8 @@ set pointsize 0.48
 set title "Tracking Original"
 set xlabel 'x'
 set ylabel 'px'
-set xrange [-0.01:0.01]
-set yrange [-0.01:0.01]
+set xrange [-0.03:0.03]
+set yrange [-0.03:0.03]
 set grid
 plot 'tracking_nica_g_13one' using 3:($1==1 ? $4 : NaN) notitle with points pointtype 1, \
 'tracking_nica_g_13one' using 3:($1==2 ? $4 : NaN) notitle with points pointtype 2, \
