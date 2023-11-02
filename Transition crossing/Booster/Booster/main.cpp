7d97efe39f91b6272af6f5193077e6281dee71da
Сергей <sergey@MacBook-Pro-4.local>
2023-11-02 21:50:45 +0300

    Save all files

---
+++ Transition crossing/Booster/Booster/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Booster
+//
+//  Created by Сергей on 25.10.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
